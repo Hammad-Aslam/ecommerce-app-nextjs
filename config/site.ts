27c2eff077fe5e2246c8dbf899cac26ef8828f91
Hammad <hammadaslam382@gmail.com>
2023-10-06 22:30:25 +0500

    Add Meta Data & Site Provider

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Name",
-  description: "Description",
+  name: "Ecommerce",
+  description: "Full satck Ecommerce App",
   footer: [
     { name: "Home", href: "/" },
     { name: "About", href: "#" },
