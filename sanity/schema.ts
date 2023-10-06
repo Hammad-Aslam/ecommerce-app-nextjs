a8fd3440d308e1d73d07f13243f8c5d90926a6c6
Hammad <hammadaslam382@gmail.com>
2023-10-06 23:35:31 +0500

    Add Sanity Integ. & Product Schema

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,5 +1,7 @@
 import { type SchemaTypeDefinition } from "sanity"
 
+import { product } from "./schemas/product-schema"
+
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [product],
 }
