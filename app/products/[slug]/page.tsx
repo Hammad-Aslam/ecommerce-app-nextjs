3118b162daabd2f5977d82293dc09169bc355278
Hammad <hammadaslam382@gmail.com>
2023-10-09 22:59:52 +0500

    Add Product Image Gallery

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -33,6 +33,7 @@ export default async function Page({ params }: Props) {
         {/* Product */}
         <div className="pb-20 lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-12">
           {/* Product gallery */}
+          <ProductGallery product={product} />
           <ProductInfo product={product} />
           {/* Product info */}
         </div>
